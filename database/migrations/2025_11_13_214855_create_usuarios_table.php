cae0527889d011de870d2220a393580c7bc9067a
Gustavo Rafael Nunes Duraes <gustavo.ipatinga@gmail.com>
2025-11-13 20:09:49 -0300

    criacao da classe usuário

--- database/migrations/2025_11_13_214855_create_usuarios_table.php
+++ database/migrations/2025_11_13_214855_create_usuarios_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +11,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // id: Long (BigInt)
+            $table->string('nome', 45);
+            $table->string('email', 250)->unique();
+            $table->string('telefone', 16);
+            $table->boolean('whatsApp')->default(false);
+            $table->date('nascimento');
+            $table->tinyInteger('idade'); // idade: byte
+            
+            // Isto vai usar os nomes 'criacao' e 'ultimaAtualizacao'
+            // que definimos na classe Entidade.php
+            $table->timestamps(); 
         });
     }
 
